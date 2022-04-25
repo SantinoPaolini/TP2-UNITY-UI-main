7894f199221ddb53c5f968f9d619ef9c6da65ceb
SantinoPaolini <101577619+SantinoPaolini@users.noreply.github.com>
2022-04-25 20:04:44 -0300

    Initial commit

---
+++ Assets/Scripts/MayoríadeEdad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MayoríadeEdad : MonoBehaviour
+{
+    public int edadusuario;
+    public Text mitexto;
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+            mitexto.text = "Es mayor de edad";
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+            mitexto.text = "No es mayor de edad";
+
+        }
+    }
+}
